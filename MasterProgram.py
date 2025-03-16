798949aed4d69177952491f3809c68a460ca726b
Firesty123 <firestriker41@gmail.com>
2025-03-16 14:47:15 +0800

    Added Remaining Route 4 - 8

--- MasterProgram.py
+++ MasterProgram.py
@@ -9,13 +9,18 @@ from ACL_FLL_v03 import *
 from Route1 import *
 from Route2 import *
 from Route3 import *
+from Route4 import *
+from Route5 import *
+from Route6 import *
+from Route7 import *
+from Route8 import *
 
 laura = Laura() #create object
 
 ######################## Total route ########################
 
 CURRENT_ROUTE = 1
-MAX_ROUTE = 3 # modify accordingly
+MAX_ROUTE = 8 # modify accordingly
 
 ######################## Main program ########################
 
@@ -41,16 +46,38 @@ while True:
             Route2()
         elif CURRENT_ROUTE == 3:
             Route3()
+        elif CURRENT_ROUTE == 4:
+            Route4()
+        elif CURRENT_ROUTE == 5:
+            Route5()
+        elif CURRENT_ROUTE == 6:
+            Route6()
+        elif CURRENT_ROUTE == 7:
+            Route7()
+        elif CURRENT_ROUTE == 9:
+            Route8()
 
         # auto-run next route
         if CURRENT_ROUTE < MAX_ROUTE:
             CURRENT_ROUTE += 1
 
     # Unregulated motor power = 100 to -100
+    # Add else if when adding another route
+    
     else:
         if CURRENT_ROUTE == 1:
             laura.Unregulated_AttachMotor(50, -50, 50)
         elif CURRENT_ROUTE == 2:
             laura.Unregulated_AttachMotor(-80, 30, 50)
         elif CURRENT_ROUTE == 3:
+            laura.Unregulated_AttachMotor(20, 70, 50)
+        elif CURRENT_ROUTE == 4:
+            laura.Unregulated_AttachMotor(20, 70, 50)
+        elif CURRENT_ROUTE == 5:
+            laura.Unregulated_AttachMotor(20, 70, 50)
+        elif CURRENT_ROUTE == 6:
+            laura.Unregulated_AttachMotor(20, 70, 50)
+        elif CURRENT_ROUTE == 7:
+            laura.Unregulated_AttachMotor(20, 70, 50)
+        elif CURRENT_ROUTE == 8:
             laura.Unregulated_AttachMotor(20, 70, 50)
---
+++ Route7.py
@@ -0,0 +1,53 @@
+######################## Pyricks library ########################
+
+from pybricks.parameters import Color, Direction, Stop, Icon
+from pybricks.tools import wait, StopWatch, Matrix
+
+######################## Custom program ########################
+
+from ACL_FLL_v03 import *
+laura = Laura() # create object
+
+######################## Route parallel program ########################
+
+# Example action
+# def __Route1_Arm1():
+    # laura.SingleMotor_BySeconds(PORT_LEFTATTACH, 200, 1000, Stop.BRAKE, False)
+    
+######################## Route program ########################
+
+### Starting position ###
+# Blue base - Align left wheel left side to 2nd Bold line from left
+
+
+def Route7():
+    laura.Hub_StatusLight(Color.MAGENTA)
+
+    """ Optional - Unregulated motor """
+    # laura.Unregulated_AttachMotor(50,-50)
+    # while not Button.CENTER in laura.Hub_ButtonPressed():
+    #     wait(20)
+    
+    """ Route start """
+    laura.SingleMotor_Brake(PORT_LEFTATTACH)
+    laura.SingleMotor_Brake(PORT_RIGHTATTACH)
+    laura.Hub_SpeakerBeep(500, 200) # wait 0.2s before start
+
+    """ Optional - Timer start """
+    # timer1 = StopWatch()
+    # timer1.reset()
+
+    """ Start your code here """
+    # Step 1 - Wall squaring & ...
+    laura.MoveSteering_Seconds(-150, 0, 300, True, 200) # wall squaring
+    
+
+    """ Optional - Timer end """
+    # print("Time used: ", timer1.time())
+
+######################## Route testing ########################
+
+# For individual route testing only
+# Comment it when using Master Program
+
+# Route1()
