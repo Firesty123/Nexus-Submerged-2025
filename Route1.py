1a1617e072bed1efe0192108ce51bfcd6b8f06e8
Firesty123 <firestriker41@gmail.com>
2025-06-08 14:06:05 +0800

    Added back Route 1 Program from April 6

--- Route1.py
+++ Route1.py
@@ -29,9 +29,27 @@ def Route1():
     #     wait(20)
     
     """ Route start """
-    laura.SingleMotor_Brake(PORT_LEFTATTACH)
-    laura.SingleMotor_Brake(PORT_RIGHTATTACH)
-    laura.Hub_SpeakerBeep(500, 200) # wait 0.2s before start
+    laura.MoveSteering_Seconds(-150, 0, 300, True, 200)
+    
+    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,-500,800,Stop.BRAKE,False)
+    laura.MoveStraight_Distance(400,400,750,False,True,Stop.BRAKE,50)
+    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,500,600,Stop.BRAKE,False)
+    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,600,2000,Stop.BRAKE,True)
+    
+    laura.PointTurn_Degree(True,-310,30,False,500)
+    
+    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,500,600,Stop.BRAKE,True)
+    laura.MoveStraight_Distance(400,400,780,False,True,Stop.BRAKE,50)
+    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,-1000,2000,Stop.BRAKE,True)
+    laura.MoveStraight_Distance(400,400,330,False,False,Stop.BRAKE,50)
+    wait(100)
+    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,1000,2000,Stop.BRAKE,True)
+    #laura.SingleMotor_BySeconds(PORT_LEFTATTACH,300,500,Stop.BRAKE,False)
+    laura.LockTurn_Degree(True,PORT_RIGHTDRIVE,700,180,Stop.BRAKE,50)
+    laura.MoveSteering_Degree(True,700,-18,1300,Stop.COAST,20)
+    
+    laura.SingleMotor_Brake(PORT_LEFTDRIVE)
+    laura.SingleMotor_Brake(PORT_RIGHTDRIVE)
 
     """ Optional - Timer start """
     # timer1 = StopWatch()
