5795c5cba8f35d9f0affba7839e96288eefeac08
Arjoonv4mp <arjoonthebest@gmail.com>
2025-06-14 13:36:44 +0800

    Route1.py

--- Route1.py
+++ Route1.py
@@ -24,41 +24,53 @@ def Route1():
     laura.Hub_StatusLight(Color.MAGENTA)
 
     """ Optional - Unregulated motor """
-    # laura.Unregulated_AttachMotor(50,-50,50) 
-    # while not Button.CENTER in laura.Hub_ButtonPressed():
-    #     wait(20)
+    laura.Unregulated_AttachMotor(50,-50,50) 
+    while not Button.CENTER in laura.Hub_ButtonPressed():
+        wait(20)
     
     """ Route start """
-    laura.MoveSteering_Seconds(-150, 0, 300, True, 200)
-    
-    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,-500,800,Stop.BRAKE,False)
-    laura.MoveStraight_Distance(400,400,750,False,True,Stop.BRAKE,50)
-    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,500,600,Stop.BRAKE,False)
-    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,600,2000,Stop.BRAKE,True)
-    
-    laura.PointTurn_Degree(True,-310,30,False,500)
-    
-    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,500,600,Stop.BRAKE,True)
-    laura.MoveStraight_Distance(400,400,780,False,True,Stop.BRAKE,50)
-    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,-1000,2000,Stop.BRAKE,True)
-    laura.MoveStraight_Distance(400,400,330,False,False,Stop.BRAKE,50)
-    wait(100)
-    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,1000,2000,Stop.BRAKE,True)
-    #laura.SingleMotor_BySeconds(PORT_LEFTATTACH,300,500,Stop.BRAKE,False)
-    laura.LockTurn_Degree(True,PORT_RIGHTDRIVE,700,180,Stop.BRAKE,50)
-    laura.MoveSteering_Degree(True,700,-18,1300,Stop.COAST,20)
-    
-    laura.SingleMotor_Brake(PORT_LEFTDRIVE)
-    laura.SingleMotor_Brake(PORT_RIGHTDRIVE)
+    laura.SingleMotor_Brake(PORT_LEFTATTACH)
+    laura.SingleMotor_Brake(PORT_RIGHTATTACH)
+    laura.Hub_SpeakerBeep(500, 200) # wait 0.2s before start
 
     """ Optional - Timer start """
     # timer1 = StopWatch()
     # timer1.reset()
 
     """ Start your code here """
-    # Step 1 - Wall squaring & ...
-    laura.MoveStraight_Distance(400,500,700,False,True,Stop.BRAKE,200)
     
+    # Step 1 - Wall Squaring
+    
+    laura.MoveStraight_Distance(300,600,-80,False,True,Stop.BRAKE,50)
+    
+    # Step 2 - Move to Kelp Forest 
+    
+    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,-300,600,Stop.BRAKE,False)
+    laura.MoveStraight_Distance(400,600,755,False,True,Stop.BRAKE,0)
+    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,300,600,Stop.BRAKE,False)
+                                
+    # Step 3 - Collect Plankton Sample
+    
+    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,800,2000,Stop.BRAKE,True)
+    
+    # Step 4 - Move to Seabed
+    
+    laura.PointTurn_Degree(True,-110,215,True,50)
+    laura.MoveStraight_Distance(400,600,300,False,True,Stop.BRAKE,0)
+
+    # Step 5 - Collect Seabed Sample
+    
+    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,-300,600,Stop.BRAKE,False)
+    laura.MoveStraight_Distance(400,600,450,False,True,Stop.BRAKE,0)
+    laura.SingleMotor_BySeconds(PORT_LEFTATTACH,-500,600,Stop.BRAKE,False)
+    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,-700,2000,Stop.BRAKE,True)
+    laura.SingleMotor_BySeconds(PORT_RIGHTATTACH,700,2000,Stop.BRAKE,False)
+    laura.MoveStraight_Distance(300,600,400,False,True,Stop.BRAKE,0)
+    
+    # Step 6 - Move to Collect Coral
+    
+    laura.LockTurn_Degree(True,PORT_RIGHTDRIVE,400,290,True,30)
+    laura.MoveStraight_Distance(1000,600,550,False,True,Stop.BRAKE,0)
 
     """ Optional - Timer end """
     # print("Time used: ", timer1.time())
@@ -68,4 +80,4 @@ def Route1():
 # For individual route testing only
 # Comment it when using Master Program
 
-#Route1()
+Route1()
